8a9c0d65f39c2af2f870f24a29120c66a1487aca
Søren Mortensen <soren@neros.dev>
2020-02-04 16:54:59 +0000

    Add database setup in startup

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -5,17 +5,42 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Persistence;
 
 namespace Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDatabaseContextFactory(options =>
+            {
+                if (this.Configuration["USE_IN_MEMORY_DATABASE"] == "TRUE")
+                {
+                    options.UseInMemoryDatabase("example_in_memory_database");
+                }
+                else if (string.IsNullOrWhiteSpace(this.Configuration["DATABASE_CONNECTION_STRING"]))
+                {
+                    throw new ArgumentException(
+                        "USE_IN_MEMORY_DATABASE was not set and no DATABASE_CONNECTION_STRING was provided.");
+                }
+                else
+                {
+                    options.UseConnectionString(this.Configuration["DATABASE_CONNECTION_STRING"]);
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,7 +51,16 @@ namespace Api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(builder => builder
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("http://localhost:3000")
+            );
+
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
