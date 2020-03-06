bd9a83fc97081063ff91e142453aa3542ad0c60f
Jake Mifsud <jm2787@bath.ac.uk>
2020-03-06 11:02:43 +0000

    change from int to enum

--- backend/Persistence/Entities/BadgeEntity.cs
+++ backend/Persistence/Entities/BadgeEntity.cs
@@ -8,6 +8,22 @@ using System.Collections.Generic;
 namespace Persistence.Entities
 {
     /// <summary>
+    /// The type of the badge, further details on what the options are are to be decided with the group.
+    /// </summary>
+    public enum Type
+    {
+        /// <summary>
+        /// Normal user authorisation level.
+        /// </summary>
+        OptionA = 1,
+
+        /// <summary>
+        /// Administrator authorisation level.
+        /// </summary>
+        OptionB,
+    }
+
+    /// <summary>
     /// Represents a Badge in the database.
     /// </summary>
     public class BadgeEntity
@@ -19,7 +35,7 @@ namespace Persistence.Entities
         /// <param name="name">The badge's Name.</param>
         /// <param name="description">The badge's Description.</param>
         /// <param name="type">The badge's type.</param>
-        public BadgeEntity(Guid id, string name, string description, int type)
+        public BadgeEntity(Guid id, string name, string description, Type type)
         {
             this.Id = id;
             this.Name = name;
@@ -45,7 +61,7 @@ namespace Persistence.Entities
         /// <summary>
         /// Gets or sets the type of this badge.
         /// </summary>
-        public int Type { get; set; }
+        public Type Type { get; set; }
 
         /// <summary>
         /// Gets or sets the user-badge relations this badge belongs to; autofilled when fetched from the
