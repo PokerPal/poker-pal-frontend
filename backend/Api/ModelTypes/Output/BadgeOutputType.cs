5d0c43411b7c4562f51345bf85d4503099ae35f8
Jake Mifsud <jm2787@bath.ac.uk>
2020-03-06 14:46:34 +0000

    Add output type and result type for badge

---
+++ backend/Api/ModelTypes/Output/BadgeOutputType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Application.Models.Output;
+
+namespace Api.ModelTypes.Output
+{
+    /// <summary>
+    /// Details about a Badge.
+    /// </summary>
+    public class BadgeOutputType
+    {
+        /// <summary>
+        /// Converts an instance of the model <see cref="BadgeOutputModel"/> to this model type.
+        /// </summary>
+        public static Func<BadgeOutputModel, BadgeOutputType> FromModel { get; } =
+            model => new BadgeOutputType { Model = model };
+
+        /// <summary>
+        /// The badge's unique identifier.
+        /// </summary>
+        public string Id => this.Model.Id.ToString();
+
+        /// <summary>
+        /// The badge's name.
+        /// </summary>
+        public string Name => this.Model.Name.ToString();
+
+        /// <summary>
+        /// Description of the badge.
+        /// </summary>
+        public string Description => this.Model.Description.ToString();
+
+        /// <summary>
+        /// The badge type.
+        /// </summary>
+        public string Type => this.Model.Type.ToString();
+
+        internal BadgeOutputModel Model { get; set; }
+    }
+}
