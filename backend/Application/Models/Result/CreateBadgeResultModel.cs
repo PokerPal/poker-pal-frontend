e464ffafa696a3bc0a82dc81b1a72e7871116592
Jake Mifsud <jm2787@bath.ac.uk>
2020-03-06 11:19:05 +0000

    Create CreateBadgeResultModel.cs

---
+++ backend/Application/Models/Result/CreateBadgeResultModel.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="CreateBadgeResultModel.cs" company="IP Group 2">
+// Copyright (c) IP Group 2. All rights reserved.
+// </copyright>
+
+namespace Application.Models.Result
+{
+    /// <summary>
+    /// Successful result of an attempt to create a new Badge.
+    /// </summary>
+    public class CreateBadgeResultModel
+    {
+        /// <summary>
+        /// The unique identifier of the created Badge.
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
