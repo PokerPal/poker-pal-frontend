db33574141aa1de5bcce87b7974bd15fb0d9dfff
Jake Mifsud <jm2787@bath.ac.uk>
2020-03-06 11:49:39 +0000

    Added some periods

--- backend/Api/ModelTypes/Input/CreateBadgeInputType.cs
+++ backend/Api/ModelTypes/Input/CreateBadgeInputType.cs
@@ -6,7 +6,7 @@
 namespace Api.ModelTypes.Input
 {
     /// <summary>
-    /// Details required to make a new badge
+    /// Details required to make a new badge.
     /// </summary>
     public class CreateBadgeInputType
     {
@@ -16,7 +16,7 @@ namespace Api.ModelTypes.Input
         public string Name { get; set; }
 
         /// <summary>
-        /// The badge's Description
+        /// The badge's Description.
         /// </summary>
         public string Description { get; set; }
     }
