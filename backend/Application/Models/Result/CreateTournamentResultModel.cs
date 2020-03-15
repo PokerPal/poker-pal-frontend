61134dc055053dc363f0efadd659e9b6a7e10eef
JakeUni <jakemifsud39@gmail.com>
2020-03-15 11:14:53 +0000

    Add tournament Service and result model

---
+++ backend/Application/Models/Result/CreateTournamentResultModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Models.Result
+{
+    /// <summary>
+    /// Successful result of an attempt to create a new Tournament.
+    /// </summary>
+    public class CreateTournamentResultModel
+    {
+        /// <summary>
+        /// The unique identifier of the created tournament.
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
