65c3a25acb16e784e2f2944950dbb04cbb4fd768
oof26 <oof26@bath.ac.uk>
2020-03-22 18:34:48 +0100

    add basic content to main league page

--- frontend/src/MainLeaguePage.js
+++ frontend/src/MainLeaguePage.js
@@ -1,11 +1,35 @@
-import logo from "./bluffBathLogo.png";
 import React from "react";
 
 export function MainLeaguePage() {
+    var pWidth=500
+    var pHeight=200
+    var pStyle = {
+        color:'#0013ae'
+    }
+    var textBoxStyle ={
+        readOnly:'true',
+        textAlign:'right'
+    }
     return (
-        <div>
-            MAIN LEAGUE PAGE
-            {<h1>TEST</h1>}
+        <div className="App">
+            <body>
+                <div className="section">
+                    <div className="leftSection">
+                        <p>Current Place</p>
+                        <input style={textBoxStyle} placeholder="10"></input>
+                        <p>Highest Place</p>
+                        <input style={textBoxStyle} placeholder="1"></input>
+                        <p>Last Updated</p>
+                        <input style={textBoxStyle} placeholder="10/12/20"></input>
+                    </div>
+                    <div className="rightSection">
+                            <p style={pStyle}>Place History</p>
+                            <svg width={pWidth} height={pHeight}>
+                                <rect width={pWidth}height={pHeight}/>
+                            </svg>                                             
+                    </div>
+                </div>
+            </body>
         </div>
     );
 }
