093dea0891e5459e7fab9435610cab61a15a91c2
Lucy Green <lsg38@bath.ac.uk>
2020-03-23 18:02:09 +0000

    created member profile

--- frontend/src/MemberProfile.js
+++ frontend/src/MemberProfile.js
@@ -1,11 +1,58 @@
 import logo from "./bluffBathLogo.png";
+import profilePicture from "./profilePicPlaceholder.jpg"
 import React from "react";
 
 export function MemberProfile() {
     return (
-        <div>
-            MEMBER PROFILE
-            {/*HTML HERE*/}
+        <div className="App">
+            <body>
+
+            <div className="side-custom-header">
+                <b>Profile</b>
+            </div>
+
+            <div className="section">
+                <div className="leftSection">
+                    <div className="sub-section-header">
+                        <b>(Member Name)</b>
+                    </div>
+
+                    <img src={profilePicture} className="profile-picture" alt="profilePicture" align="left" width="175" height="175" />
+
+                    <div className="sub-section-header">
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <b>Badges</b>
+                    </div>
+
+                    <div className="circle">
+                    </div>
+                    <div className="circle">
+                    </div>
+                    <div className="circle">
+                    </div>
+                    <div className="circle">
+                    </div>
+                </div>
+
+                <div className="rightSection">
+                    <div className="sub-section-header">
+                        <b>My Stats</b>
+                    </div>
+                    <div className="smaller-text">
+                        <p>rank = </p>
+                        <p>balance = </p>
+                        <p>wins = </p>
+                    </div>
+
+                </div>
+            </div>
+            </body>
         </div>
     );
 }
