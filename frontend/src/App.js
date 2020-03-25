9173ac939a754b49219b57faf7609fd25ec00030
james <jamesausten8@gmail.com>
2020-03-25 18:05:53 +0000

    rearrange layout alter links slightly to make content appear not in the header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import {
     Route,
     Link
 } from "react-router-dom";
-import logo from './bluffBathLogo.png';
+import BluffBathLogo from './bluffBathLogo.png';
 import './App.css';
 import './Layout.css';
 import {LoginPage} from "./LoginPage";
@@ -15,56 +15,66 @@ import {SideLeaguePage} from "./SideLeaguePage";
 import {AdminOptions} from "./AdminOptions";
 import {UserSettings} from "./UserSettings";
 
-let ContentToSee = <div> </div>;
-
 class App extends Component {
     render() {
         return (
             <div className="universalTextColour">
-
-                <div className="section">
-                    <div className="header">
-
-                        <div className="headerLeft">
-                            <img src={logo} className="App-logo-small" alt="App-logo-small"/>
-                        </div>
-
-                        <div className="headerRight">
-                            {/*<h2 className="Page-header">Main League</h2>*/}
-                            <Switcher/>
-                        </div>
-
-                    </div>
-                </div>
-
-                <hr />
-
-
-                {/*<ContentToSee/>*/}
-                <ContentChange/>
-
-
-
-                <div className="section">
-
-                    <div className="leftSection">
-                        <p>
-
-                        </p>
+                <header>
+                    <div className="headerLeft">
+                        <img src={BluffBathLogo} className="App-logo-small" alt="App-logo-small"/>
                     </div>
 
-                    {/*TODO sort layout stuff*/}
-                    {/*<div className="rightSection">
-                        <p>LOGIN </p>
-                        <ul>
-                            one
-                            two
-                            three
+                    <div className="headerRight">
+                        <ul className="navBar">
+                            <a href="/">Dashboard | </a>
+                            <a href="/login">Login | </a>
+                            <a href="/memberProfile">Profile | </a>
+                            <a href="/mainLeague">Main League | </a>
+                            <a href="/sideLeague">Side League | </a>
+                            <a href="/adminOptions">Admin options | </a>
+                            <a href="/userSettings">Settings </a>
                         </ul>
-                    </div>*/}
+                    </div>
 
-                </div>
+                </header>
 
+                <div className="hrLine"/>
+
+                <container className="section">
+
+                    <Router>
+                        <Switch>
+                            <Route exact path="/">
+                                <Dashboard />
+                            </Route>
+
+                            <Route path="/login">
+                                <LoginPage />
+                            </Route>
+
+                            <Route path="/memberProfile">
+                                <MemberProfile />
+                            </Route>
+
+                            <Route path="/mainLeague">
+                                <MainLeaguePage />
+                            </Route>
+
+                            <Route path="/sideLeague">
+                                <SideLeaguePage />
+                            </Route>
+
+                            <Route path="/adminOptions">
+                                <AdminOptions />
+                            </Route>
+
+                            <Route path="/userSettings">
+                                <UserSettings />
+                            </Route>
+                        </Switch>
+                    </Router>
+
+                </container>
             </div>
 
         );
@@ -73,77 +83,10 @@ class App extends Component {
 
 export default App;
 
-function ContentChange(props){
-    console.log(props.thing);
-    if (props.thing==="LoginPage") {
-        ContentToSee = <LoginPage/>;
-    }else if (props.thing==="MemberProfile") {
-
-    }
-
-
-    return (ContentToSee)
-}
-
-function Switcher(){
-    return(
-        <div>
-        <Router>
-            <div className="navBar">
-                <Link to="/">  Home | </Link>
-                <Link to="/login">  Login | </Link>
-                <Link to="/memberProfile">  Profile | </Link>
-                <Link to="/mainLeague">  Main League | </Link>
-                <Link to="/sideLeague">  Side League | </Link>
-                <Link to="/adminOptions">  Admin Options | </Link>
-                <Link to="/userSettings">  User Settings </Link>
-            </div>
-            {/*<hr />*/}
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-
-                <Route path="/login">
-                    {/*<LoginPage />*/}
-                    <ContentChange thing="LoginPage"/>
-                    {/*<ContentChange toSee={LoginPage}/>*/}
-                </Route>
-
-                <Route path="/memberProfile">
-                    <ContentChange thing="fart"/>
-                    <MemberProfile />
-                </Route>
-
-                <Route path="/mainLeague">
-                    <MainLeaguePage />
-                </Route>
-
-                <Route path="/sideLeague">
-                    <SideLeaguePage />
-                </Route>
-
-                <Route path="/adminOptions">
-                    <AdminOptions />
-                </Route>
-
-                <Route path="/userSettings">
-                    <UserSettings />
-                </Route>
-
-
-            </Switch>
-        </Router>
-        </div>
-    )
-}
-
-
-
-function Home() {
+function Dashboard() {
     return (
         <div>
-            <h2>Home</h2>
+            <h2>Dashboard</h2>
         </div>
     );
 }
