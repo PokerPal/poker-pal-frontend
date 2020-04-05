ee9234ea9420c6a069a042b732b336178e919954
james <jamesausten8@gmail.com>
2020-04-05 13:35:12 +0100

    stuff I forgot to commit last time I was here

--- frontend/src/AdminOptions.js
+++ frontend/src/AdminOptions.js
@@ -1,11 +1,60 @@
 import logo from "./bluffBathLogo.png";
 import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {MainLeaguePage} from "./MainLeaguePage";
+
 
 export function AdminOptions() {
-    return (
-        <div>
-            ADMIN OPTIONS
-            {/*HTML HERE*/}
-        </div>
-    );
+  return (
+    <div>
+
+
+      <Router>
+        <Switch>
+          <Route exact path="/adminOptions">
+            <MainScreen />
+          </Route>
+
+          <Route exact path="/adminOptions/enterMainLeague">
+            <EnterMainLeagueData />
+          </Route>
+
+          <Route exact path="/adminOptions/enterSideLeague">
+            <EnterSideLeagueData />
+          </Route>
+
+        </Switch>
+      </Router>
+
+    </div>
+
+
+  );
+}
+
+function MainScreen() {
+  return(
+    <div>
+      ADMIN OPTIONS
+      <input type="button"/><label className="adminButtons"><a href="/adminOptions/enterMainLeague">Enter places in main league</a></label>
+      <input type="button"/><label className="adminButtons"><a href="/adminOptions/enterSideLeague">Enter in/out in side league</a></label>
+      <br/>
+    </div>
+  )
+}
+
+function EnterMainLeagueData() {
+  return(
+    <div>
+      Enter Main League Data
+    </div>
+  )
+}
+
+function EnterSideLeagueData() {
+  return(
+    <div>
+      Enter Side League Data
+    </div>
+  )
 }
