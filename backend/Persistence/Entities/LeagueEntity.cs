398deb94c1b32c4c2bf306c089200dffccb0e8ed
Jake Mifsud <jm2787@bath.ac.uk>
2020-04-06 19:54:07 +0100

    Update LeagueEntity in preperation for userleagues

--- backend/Persistence/Entities/LeagueEntity.cs
+++ backend/Persistence/Entities/LeagueEntity.cs
@@ -11,7 +11,7 @@ namespace Persistence.Entities
         /// <summary>
         /// Initializes a new instance of the <see cref="LeagueEntity"/> class.
         /// </summary>
-        /// <param name="id">The league's unique identifier.</param>
+        /// <param name="id">The league's unsique identifier.</param>
         /// <param name="name">The league's name.</param>
         public LeagueEntity(int id, string name)
         {
@@ -31,9 +31,25 @@ namespace Persistence.Entities
         public string Name { get; set; }
 
         /// <summary>
+        /// Gets or sets this league's starting amount, the amount a user starts with.
+        /// </summary>
+        public int StartingAmount { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether or not events in this league can be changed.
+        /// </summary>
+        public bool AllowChanges { get; set; }
+
+        /// <summary>
         /// Gets or sets the list of sessions that have occurred in this league; autofilled when
         /// fetched from the database, null otherwise.
         /// </summary>
         public virtual List<SessionEntity> Sessions { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the list of user leagues that have occurred in this league; autofilled when
+        /// fetched from the database, null otherwise.
+        /// </summary>
+        public virtual IEnumerable<UserLeagueEntity> UserLeagues { get; set; } = null;
     }
 }
