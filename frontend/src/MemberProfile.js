df929db63deaa0030d2e45059ac92b77675efd82
Lucy Green <lsg38@bath.ac.uk>
2020-04-06 13:37:56 +0100

    add chart to member profile

--- frontend/src/MemberProfile.js
+++ frontend/src/MemberProfile.js
@@ -6,8 +6,23 @@ import fifty from "./50.png"
 
 import React from "react";
 import Cookies from 'universal-cookie';
+import './Tournaments.css';
+import {Line} from 'react-chartjs-2';
+import MainLeagueLeaderboard from "./MainLeagueLeaderboard";
 
 export function MemberProfile() {
+    var hPlace = 10 //NEED TO GET FROM API
+    var cPlace = 10 //NEED TO GET FROM API
+    var lastUpdate = "11/10/20"
+    var pHistory = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"], //WHAT DO WE WANT ON AXIS?
+        datasets: [{
+            label : 'Place History',
+            backgroundColor: '#0013ae',
+            borderColor: '#0013ae',
+            data: [0, 10, 5, 2, 20, 30, 45], //NEED TO GET FROM API
+        }]
+    }
     return (
         <div className="App">
             <div className="side-custom-header">
@@ -37,16 +52,25 @@ export function MemberProfile() {
                         <b>Badges</b>
                     </div>
 
-                    <img src={medal} className="circle" alt="circle" align="left"  />
+
+                    <img src={medal} className="circle" alt="circle" align="left"/>
                     <img src={dunce} className="circle" alt="circle" align="left"  />
                     <img src={nine} className="circle" alt="circle" align="left"  />
                     <img src={fifty} className="circle" alt="circle" align="left"  />
                 </div>
 
-                <div className="rightSection">
+                <div className="tournamentRightSection">
+
+
                     <div className="sub-section-header">
                         <b>My Stats</b>
                     </div>
+
+
+                    <p><strong>Place History</strong></p>
+                    <Line data={pHistory}/>
+
+
                     <div className="smaller-text">
                         {/*<p>rank = </p>*/}
                         <GetRank/>
