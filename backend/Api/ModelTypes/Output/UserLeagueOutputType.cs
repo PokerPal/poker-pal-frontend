d72647b47520cabd4896311d8170579391fb855f
Jake Mifsud <jm2787@bath.ac.uk>
2020-04-06 20:48:52 +0100

    Add user league output model and type

---
+++ backend/Api/ModelTypes/Output/UserLeagueOutputType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Application.Models.Output;
+
+namespace Api.ModelTypes.Output
+{
+    /// <summary>
+    /// Details about a user league.
+    /// </summary>
+    public class UserLeagueOutputType
+    {
+        /// <summary>
+        /// Converts an instance of the model <see cref="UserLeagueOutputModel"/> to this output
+        /// type.
+        /// </summary>
+        public static readonly Func<UserLeagueOutputModel, UserLeagueOutputType> FromModel
+            = model => new UserLeagueOutputType { Model = model };
+
+        /// <summary>
+        /// The users unique identifier.
+        /// </summary>
+        public int UserId => this.Model.UserId;
+
+        /// <summary>
+        /// The leagues unique identifier.
+        /// </summary>
+        public int LeagueId => this.Model.LeagueId;
+
+        /// <summary>
+        /// The user's total score in this league.
+        /// </summary>
+        public int TotalScore => this.Model.TotalScore;
+
+        internal UserLeagueOutputModel Model { get; set; }
+    }
+}
