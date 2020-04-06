b6a93b91c8cdd363288ba94682077a635ede9069
Jake Mifsud <jm2787@bath.ac.uk>
2020-04-06 19:54:22 +0100

    Add user leagues, update db config

--- backend/Persistence/Entities/UserLeagueEntity.cs
+++ backend/Persistence/Entities/UserLeagueEntity.cs
@@ -6,6 +6,17 @@
     public class UserLeagueEntity
     {
         /// <summary>
+        ///  Initializes a new instance of the <see cref="UserLeagueEntity"/> class.
+        /// </summary>
+        /// <param name="userId">The users unique identification.</param>
+        /// <param name="leagueId">The leagues unique identification</param>
+        public UserLeagueEntity(int userId, int leagueId)
+        {
+            this.UserId = userId;
+            this.LeagueId = leagueId;
+        }
+
+        /// <summary>
         /// Gets or sets the User ID.
         /// </summary>
         public int UserId { get; set; }
@@ -19,5 +30,17 @@
         /// Gets or sets the total score.
         /// </summary>
         public int TotalStore { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user entity linked to this entity; autofilled when fetched from the
+        /// database, null otherwise.
+        /// </summary>
+        public virtual UserEntity User { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the league this session belongs to; autofilled when fetched from the
+        /// database, null otherwise.
+        /// </summary>
+        public virtual LeagueEntity League { get; set; } = null;
     }
 }
