00481a267149986f1491e7f60e389dfcf077bae9
Jake Mifsud <jm2787@bath.ac.uk>
2020-04-06 19:14:58 +0100

    Create UserLeagueEntity.cs

---
+++ backend/Persistence/Entities/UserLeagueEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace Persistence.Entities
+{
+    /// <summary>
+    /// The users status in a league
+    /// </summary>
+    public class UserLeagueEntity
+    {
+        /// <summary>
+        /// Gets or sets the User ID.
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the League ID.
+        /// </summary>
+        public int LeagueId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total score.
+        /// </summary>
+        public int TotalStore { get; set; }
+    }
+}
