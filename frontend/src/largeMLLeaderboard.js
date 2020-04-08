0674e1951c0c8a2749da0be57ada44b9a1e4dc28
oof26 <oof26@bath.ac.uk>
2020-04-08 23:09:27 +0200

    remove useless comments

--- frontend/src/largeMLLeaderboard.js
+++ frontend/src/largeMLLeaderboard.js
@@ -3,21 +3,16 @@ import './Tournaments.css'
 import axios from 'axios'
 import Table from './Table'
 export function  LargeMLLeaderboard(){
-    // data state to store the TV Maze API data. Its initial value is an empty array
   const [data, setData] = useState([]);
 
-  // Using useEffect to call the API once mounted and set the data
   useEffect(() => {
     (async () => {
       const result = await axios("http://localhost:5000/leagues/1/users");
       setData(result.data.value);
     })();
   }, []);
-  console.log(data);
-  /* 
-    - Columns is a simple array right now, but it will contain some logic later on. It is recommended by react-table to Memoize the columns data
-    - Here in this example, we have grouped our columns into two headers. react-table is flexible enough to create grouped table headers
-  */
+
+  
   const columns = useMemo(
     () => [
       {
