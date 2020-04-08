bc5d33356386e1f0a35fc515d454a97c86e32032
Lucy Green <lsg38@bath.ac.uk>
2020-04-08 12:37:34 +0100

    contrast box final

--- frontend/src/MemberProfile.js
+++ frontend/src/MemberProfile.js
@@ -63,17 +63,11 @@ export function MemberProfile() {
                     <br></br>
 
                     <link id="pagestyle" rel="stylesheet" type="text/css" href="App.css"/>
-
                     <p id="button" onClick="ChangeContrast('AppContrast.css')">
                         <div className="button">
                             <b>High contrast mode</b>
                         </div>
                     </p>
-                    
-
-                    <input type="checkbox" id="checkbox" name="highContrast" value="contrast"/>
-                        <label htmlFor="uni"> uni </label>
-
                 </div>
 
                 <div className="memberRightSection">
@@ -112,7 +106,8 @@ function Reload(){
 
 function ChangeContrast(sheet){
     document.body.style.background= "#FCFF90";
-    document.body.style.fontWeight= "bold";
+    document.body.style.fontWeight= "bolder";
+    document.body.style.fontSize = "100px"
     //document.body.style.cssText = "color: black";
     document.getElementById('pagestyle').setAttribute('href', sheet);
 }
