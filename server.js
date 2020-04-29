905ef6a9b5881304ea1f624ee1c28b02de2c41a2
Søren Mortensen <soren@neros.dev>
2020-04-29 13:30:11 +0100

    Change default port in server.js to 3000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(favicon(__dirname + '/build/favicon.ico'));
